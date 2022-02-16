7af652543d3c8288eb2a83684ea9ed2d4fac0165
Domiciano Rincon Niño <1143848922@icesi.edu.co>
2022-02-16 08:36:12 -0500

    Semana 4

---
+++ src/control/VentanaB.java
@@ -0,0 +1,27 @@
+package control;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+
+public class VentanaB implements Initializable{
+
+    @FXML
+    private TextArea outputTA;
+    
+    private String email;
+    
+    public VentanaB(String email) {
+    	this.email = email;
+    }
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		outputTA.setText(email);
+		
+	}
+
+}
